518c0b8fbec70988507259b9c803c069c3428764
PyMan142 <91704113+PyMan142@users.noreply.github.com>
2021-10-03 16:08:58 -0400

    Updated Functions from breaking

--- functions.py
+++ functions.py
@@ -8,19 +8,22 @@ import time
 
 def resetCursor2():
     time.sleep(2)
-    pyautogui.keyDown('tab')
+    pydirectinput.keyDown('tab')
+    pydirectinput.keyUp('tab')
     time.sleep(2)
     x, y = pyautogui.locateCenterOnScreen('1.png')
     time.sleep(0.3)
     pydirectinput.click(x, y)  
     time.sleep(1)
     pydirectinput.keyDown('esc')
+    pydirectinput.keyUp('esc')
     time.sleep(1)
     pydirectinput.moveRel(175, None)
 
 def repairPole():
     time.sleep(2)
-    pyautogui.keyDown('tab')
+    pydirectinput.keyDown('tab')
+    pydirectinput.keyUp('tab')
     time.sleep(2)
     a, b = pyautogui.locateCenterOnScreen('f3.png')
     time.sleep(0.3)
@@ -37,6 +40,7 @@ def repairPole():
     pyautogui.keyDown('e')
     time.sleep(1)
     pydirectinput.keyDown('esc')
+    pydirectinput.keyUp('esc')
     time.sleep(2)
     print('Pole repaired')
     time.sleep(2)
